fef6c001d5f739c579530cf05b375f8bf3c06dc0
DeepikaaAPA <deepikaarjun@gmail.com>
2024-09-02 22:26:46 +0530

    add:reset password

---
+++ src/components/ResetPassword.jsx
@@ -0,0 +1,59 @@
+import React, { useState } from "react";
+import axios from "axios";
+
+const ResetPassword = ({ email }) => {
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+      return;
+    }
+
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:3000/api/v1/resetPassword`,
+        { email, password }
+      );
+      setMessage(response.data.message);
+    } catch (error) {
+      setMessage(error.data.message || "An error occurred");
+    }
+  };
+
+  return (
+    <div className="p-5  container border">
+      <h2 className="text-primary">Reset Password</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>New Password:</label>
+          <input
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+        </div>
+
+        <div>
+          <label>Confirm Password:</label>
+          <input
+            type="password"
+            value={confirmPassword}
+            onChange={(e) => setConfirmPassword(e.target.value)}
+            required
+          />
+        </div>
+        <button className="m-5 btn btn-primary" type="submit">
+          Reset Password
+        </button>
+      </form>
+      {message && <p className="text-warning">{message}</p>}
+    </div>
+  );
+};
+
+export default ResetPassword;
